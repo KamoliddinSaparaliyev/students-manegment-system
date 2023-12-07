dc32a95ba6992792e50034adc45fc8e508032c73
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-12-07 23:00:02 +0500

    Fix code and config file

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,18 @@ const config = {
   port: process.env.PORT,
   env: process.env.NODE_ENV,
   mongo_uri: process.env.MONGO_URI,
+  geocoder: {
+    provider: process.env.GEOCODER_PROVIDER,
+    apiKey: process.env.GEOCODER_API_KEY,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expire: process.env.JWT_EXPIRE,
+    cookie_expire: process.env.JWT_COOKIE_EXPIRE,
+  },
+  email: process.env.EMAIL,
+  pass: process.env.PASSWORD,
+  service: process.env.SERVICE,
 };
 
-module.exports = config;
+module.exports = { config };
