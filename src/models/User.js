a6dbc9f03c2d2c892a8eb14382b79f5e6617b932
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-12-07 22:58:59 +0500

    Fix code and added other fields

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,9 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
+const { genSalt, hash, compare } = require("bcryptjs");
+const { sign } = require("jsonwebtoken");
+const { config } = require("../config/config");
+const { geocoder } = require("../utils/geocoder");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -22,6 +27,16 @@ const UserSchema = new mongoose.Schema(
       minlenght: 6,
       select: false,
     },
+    age: {
+      type: Number,
+      required: [true, "Please add a age"],
+      validate: {
+        validator: function (v) {
+          return v.toString().length === 2;
+        },
+        message: "Please, enter a correct age!",
+      },
+    },
     phoneNumber: {
       type: Number,
       unique: true,
@@ -35,11 +50,17 @@ const UserSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      enum: ["male", "female"],
+      required: false,
+      default: "Choose",
+      enum: ["male", "female", "Choose"],
     },
     photo: {
       type: String,
       default: "no-photo.jpg",
+    },
+    address: {
+      type: String,
+      required: [true, "Please add a address"],
     },
     location: {
       type: {
@@ -57,6 +78,10 @@ const UserSchema = new mongoose.Schema(
       zipcode: String,
       country: String,
     },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
   },
@@ -67,6 +92,65 @@ const UserSchema = new mongoose.Schema(
     },
   }
 );
+
+// Hashing password
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+
+  const salt = await genSalt(10);
+
+  this.password = await hash(this.password, salt);
+});
+
+// Sign JWT token and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return sign({ user: { id: this._id } }, config.jwt.secret, {
+    expiresIn: config.jwt.expire,
+  });
+};
+
+// Compare password
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await compare(enteredPassword, this.password);
+};
+
+// Assuming UserSchema is defined correctly...
+UserSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hash token and set resetPasswordToken field on the instance
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set the expiration time for the reset token
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000; // 10 minutes in milliseconds
+
+  // Save the changes made to the user document
+  this.save({ validateBeforeSave: false });
+
+  return resetToken;
+};
+
+//Geocode & location field
+UserSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  this.address = undefined;
+  next();
+});
 
 const User = mongoose.model("User", UserSchema);
 module.exports = { User };
