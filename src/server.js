bca5a24cf010d19a04bc58c2f3419af949513c6b
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2023-12-08 14:58:40 +0500

    Use router

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const colors = require("colors");
-const config = require("./config/config");
+const morgan = require("morgan");
 const connectDB = require("./config/db");
+const { config } = require("./config/config");
+const authRouter = require("./routes/auth");
+const { errorHandler } = require("./middleware/error");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
 //Connect to Database
 connectDB();
+
+// JSON parse
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Morhan
+app.use(morgan("dev"));
+
+//Routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", usersRouter);
+
+// Error handling
+app.use(errorHandler);
 
 const PORT = config.port;
 
